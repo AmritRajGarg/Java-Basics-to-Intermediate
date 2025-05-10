0d522b0b674e8095a2ff978ffe8c48425f1ad579
Amrit Raj Garg <166982958+AmritRajGarg@users.noreply.github.com>
2025-05-10 13:29:10 +0530

    Create PrintNumAndText.java

---
+++ Lab1/PrintNumAndText.java
@@ -0,0 +1,10 @@
+//WAP to print a number and a text using Java, use "print"
+
+class PrintNumAndText {
+    public static void main(String[] args) {
+        int number = 420;
+        String text = "Hello!";
+
+        System.out.print("Number: " + number + ", Text: " + text);
+    }
+}
