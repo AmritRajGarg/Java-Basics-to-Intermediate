fbca3afd1ecd39d40a9d2e4da039d5d87fc68b8f
Amrit Raj Garg <166982958+AmritRajGarg@users.noreply.github.com>
2025-06-02 23:39:51 +0530

    Create simple2.java

---
+++ Lab5/simple2.java
@@ -0,0 +1,21 @@
+/*
+Wap in Java
+Create me a program simple2.java and put it in mypack 
+Create a custom method void disp() that print "this is simple2" 
+Create simple3 and in same package and call void disp from simple2 
+Create simple4 and put it in differnet package 
+Call void disp from simple2 using import 
+*/
+
+
+// File: mypack/Simple2.java
+package mypack;
+
+public class Simple2 {
+public static void main(String[] args) {
+        System.out.println("This is a simple2");
+    }
+    public void disp() {
+        System.out.println("This is simple2");
+    }
+}
