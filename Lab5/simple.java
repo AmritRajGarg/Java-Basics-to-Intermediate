4a6c118e9a30416baf7fe6872d9bce3f9f7b6952
Amrit Raj Garg <166982958+AmritRajGarg@users.noreply.github.com>
2025-06-02 23:39:00 +0530

    Create simple.java

---
+++ Lab5/simple.java
@@ -0,0 +1,10 @@
+//WAP in java to create a package
+
+package mypackage;
+
+public class simple {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the package!");
+    }
+}
