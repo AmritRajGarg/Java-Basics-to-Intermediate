b710e05250b4f565dfaeaca4d5ee4bd4e9fbb375
Amrit Raj Garg <166982958+AmritRajGarg@users.noreply.github.com>
2025-06-10 22:31:10 +0530

    Create MyClass.java

---
+++ Lab11/MyClass.java
@@ -0,0 +1,16 @@
+/* 
+will update later
+*/
+
+
+public class MyClass {
+    public static void main(String[] args) {
+        String s1 = new String("String 1");
+        StringBuffer s2 = new StringBuffer("String 2");
+
+        System.out.println("First string: " + s1);
+        System.out.println("Second string: " + s2);
+        System.out.println("Lowercase of s1: " + s1.toLowerCase());
+        System.out.println("Lowercase of s2: " + s2.toString().toLowerCase());
+    }
+}
